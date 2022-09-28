27c9a4555d9d768e8ce5a769302532e1c747db08
iki <iki.jito@gmail.com>
2022-09-28 20:38:44 +0800

    multipg1 initial commit

---
+++ multiplayerGame1/server/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+const server = http.createServer(app);
+
+server.listen(3001, () => {
+  console.log("server is running");
+});
