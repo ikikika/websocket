2d5cb1d01b1bc5e17b19cd7f2f5458e6c7dfba62
iki <iki.jito@gmail.com>
2023-01-01 13:50:00 +0800

    creating a first class

--- classes-interfaces/src/app.ts
+++ classes-interfaces/src/app.ts
@@ -1,1 +1,10 @@
-// Code goes here!
+class Department {
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
+}
+
+// create a new js object based on the blueprint
+const accounting = new Department("Accounting");
