21e5d7b691fa9eb0435d3d4802aeb3b5f14b702a
iki <iki.jito@gmail.com>
2023-01-01 14:04:10 +0800

    private and public access modifiers

--- classes-interfaces/src/app.ts
+++ classes-interfaces/src/app.ts
@@ -1,5 +1,8 @@
 class Department {
-  name: string;
+  public name: string;
+
+  // to protect this array so that we can control how it is accessed/modified
+  private employees: string[] = [];
 
   constructor(n: string) {
     this.name = n;
@@ -11,15 +14,35 @@ class Department {
   describe(this: Department) {
     console.log("Department: " + this.name);
   }
+
+  // because employee is private, we define this function as the only way to add items to that array
+  addEmployee(employee: string) {
+    // validation etc
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
 }
 
 // create a new js object based on the blueprint
 const accounting = new Department("Accounting");
 
+accounting.addEmployee("Max");
+accounting.addEmployee("Manu");
+
+// this will not work now after we make the employee array private
+// accounting.employees[2] = 'Anna';
+
 // access a method in a class
 accounting.describe();
 
-// typescript sees that this structure is similiar to the Department class, and it would infer it.
-const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+accounting.name = "NEW NAME";
+accounting.printEmployeeInformation();
 
-accountingCopy.describe();
+// typescript sees that this structure is similiar to the Department class, and it would infer it.
+// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+
+// accountingCopy.describe();
