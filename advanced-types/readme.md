9bbb64756aca34dbbc1ef475f4b20781c08bb479
iki <iki.jito@gmail.com>
2023-01-05 04:15:16 +0800

    type guards

--- advanced-types/readme.md
+++ advanced-types/readme.md
@@ -1,3 +1,12 @@
 ## Intersection types
 
 - allow us to combine other types
+
+## Type guards
+
+- helps us with union types
+- used to check if certain property or method exists before trying to use it
+- approaches: use if statements with
+  - typeof (other types)
+  - in (objects)
+  - instanceof (objects)
--- advanced-types/src/app.ts
+++ advanced-types/src/app.ts
@@ -26,3 +26,64 @@ type Combinable = string | number;
 type Numeric = number | boolean;
 
 type Universal = Combinable & Numeric;
+
+function add(a: Combinable, b: Combinable) {
+  // this is a type guard using if
+  if (typeof a === "string" || typeof b === "string") {
+    return a.toString() + b.toString();
+  }
+  return a + b;
+}
+
+type UnknownEmployee = Employee | Admin;
+
+function printEmployeeInformation(emp: UnknownEmployee) {
+  console.log("Name: " + emp.name);
+  // typeof emp is object. not helpful
+  // type guard using in to check if property exist in object. write property as string.
+  if ("privileges" in emp) {
+    console.log("Privileges: " + emp.privileges);
+  }
+  if ("startDate" in emp) {
+    console.log("Start Date: " + emp.startDate);
+  }
+}
+
+printEmployeeInformation({ name: "Manu", startDate: new Date() });
+
+class Car {
+  drive() {
+    console.log("Driving...");
+  }
+}
+
+class Truck {
+  drive() {
+    console.log("Driving a truck...");
+  }
+
+  loadCargo(amount: number) {
+    console.log("Loading cargo ..." + amount);
+  }
+}
+
+type Vehicle = Car | Truck;
+
+const v1 = new Car();
+const v2 = new Truck();
+
+function useVehicle(vehicle: Vehicle) {
+  vehicle.drive();
+
+  // this will work
+  //   if( 'loadCargo' in vehicle ){
+
+  // type guard using instanceof.
+  // better approach as eliminates risk of spelling error
+  if (vehicle instanceof Truck) {
+    vehicle.loadCargo(1000);
+  }
+}
+
+useVehicle(v1);
+useVehicle(v2);
