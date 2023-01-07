8e59e886bccb3eaa7a1e94cbbbaf2c6d897a1f52
iki <iki.jito@gmail.com>
2023-01-07 21:52:22 +0800

    built in generics

---
+++ generics/README.md
@@ -0,0 +1,6 @@
+## What
+
+- A generic type is a type which is kind of connected with some other type and is really flexible regarding which exact type that other type is.
+- eg, Array, inside array can contain other types
+  - If TS knows that the array hold strings, TS can then allow us to perform string methods
+- Another eg, promises
--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,1 +1,13 @@
-// Code goes here!
+const names: Array<string> = []; // string[]
+names[0].split(" ");
+
+const promise: Promise<number> = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(10);
+  }, 2000);
+});
+
+promise.then((data) => {
+  // this wont work as we defined return type number
+  // data.split(" ");
+});
