de173d61991cd1456d1a52bb4ebd3b7b54620680
iki <iki.jito@gmail.com>
2023-01-08 08:42:30 +0800

    generics with constraints

--- generics/README.md
+++ generics/README.md
@@ -4,3 +4,8 @@
 - eg, Array, inside array can contain other types
   - If TS knows that the array hold strings, TS can then allow us to perform string methods
 - Another eg, promises
+
+## Creating generic function
+
+- enclosed in angle brackets
+- pass extra information to TS so that TS can better infer the result type
--- generics/src/app.ts
+++ generics/src/app.ts
@@ -1,13 +1,25 @@
-const names: Array<string> = []; // string[]
-names[0].split(" ");
+// const names: Array<string> = []; // string[]
+// names[0].split(" ");
 
-const promise: Promise<number> = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    resolve(10);
-  }, 2000);
-});
+// const promise: Promise<number> = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     resolve(10);
+//   }, 2000);
+// });
 
-promise.then((data) => {
-  // this wont work as we defined return type number
-  // data.split(" ");
-});
+// promise.then((data) => {
+//   // this wont work as we defined return type number
+//   // data.split(" ");
+// });
+
+// need to define the generics within the angle brackets else TS wont know whats contained in objA and objB
+// this is not specific enough for TS to infer that the return should be intersection of object and object, intersection of 2 unknown objects is jus another unknown objects
+// function merge(objA: object, objB: object) {
+// this way specifically tell TS that return is intersection of the 2 objects
+// also constraints T and U to be any type
+function merge<T extends object, U extends object>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+
+const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
+console.log(mergedObj.age);
