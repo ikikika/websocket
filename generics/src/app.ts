c0872fa8858638613fb467d0a8eb096c14125e51
iki <iki.jito@gmail.com>
2023-01-08 15:35:50 +0800

    another generic function example

--- generics/src/app.ts
+++ generics/src/app.ts
@@ -23,3 +23,22 @@ function merge<T extends object, U extends object>(objA: T, objB: U) {
 
 const mergedObj = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 console.log(mergedObj.age);
+
+interface Lengthy {
+  length: number;
+}
+
+// dun need to care if element is string or array
+// only care that element has length property
+// no ned to wirte overloads to check if element is string or array
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "Got no value.";
+  if (element.length === 1) {
+    descriptionText = "Got 1 element.";
+  } else if (element.length > 1) {
+    descriptionText = "Got " + element.length + " elements.";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe(["Sports", "Cooking"]));
