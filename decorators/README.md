bb803a2e340349c1559b8c356041b040176f2cb7
iki <iki.jito@gmail.com>
2023-01-09 23:32:17 +0800

    decorator function

--- decorators/README.md
+++ decorators/README.md
@@ -2,3 +2,4 @@
 
 - a function applied to something (eg a class) in a certain way
 - TS recognise the @ symbol and point to the function after it, but not execute it
+- a function that hook into the source code and either extend the function of it or annotate it with metadata
--- decorators/src/app.ts
+++ decorators/src/app.ts
@@ -1,9 +1,12 @@
-function Logger(constructor: Function) {
-  console.log("Logging...");
-  console.log(constructor);
+function Logger(logString: string) {
+  // decorator factory return a function in the decorator function
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-@Logger
+@Logger("LOGGING - PERSON")
 class Person {
   name = "Max";
 
